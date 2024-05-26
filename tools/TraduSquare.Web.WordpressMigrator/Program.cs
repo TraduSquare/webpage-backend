c2094a8d552093f8939c4ab8898eb4197ead0b37
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-05-27 00:45:56 +0200

    :tada: Setup migration projects

---
+++ tools/TraduSquare.Web.WordpressMigrator/Program.cs
@@ -0,0 +1,17 @@
+﻿using TraduSquare.Web.Rest.Client;
+using TraduSquare.Web.Rest.Projects;
+
+Console.WriteLine("Hello");
+
+const string Token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUcmFkdVNxdWFyZSIsImlhdCI6MTcxNjU4NjU3MiwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiam9ndWVyc2FuQGdtYWlsLmNvbSIsIkdpdmVuTmFtZSI6IkpvcmdlIiwiU3VybmFtZSI6Ikd1ZXJyYSIsIkVtYWlsIjoiam9ndWVyc2FuQGdtYWlsLmNvbSIsIlJvbGVzIjpbIlJlYWQiLCJXcml0ZSJdfQ.qjZDtWWlNBKVtTcHI4gsqdeTlgyYsV1MXZsVOiwH5sU";
+var client = new TraduSquareRestClient("http://localhost:2300", Token);
+
+var projectInfo = new CreateProjectRequest {
+    Project = new CreateProjectInfo {
+        Title = "Pokémon Conqueset",
+        Slug = "pokemon-conquest",
+        Description = "Cool game",
+    },
+};
+CreateProjectResponse? response = await client.Projects.CreateAsync(projectInfo);
+Console.WriteLine(response);
